9f260d4b8062e59af1fe3641b5d57ccd927a83d9
Manish <105487478+Manish105@users.noreply.github.com>
2023-05-08 19:11:00 -0400

    Arduino sketch files

---
+++ rasterize_stroke.h
@@ -0,0 +1,10 @@
+
+void RasterizeStroke(
+    int8_t* stroke_points,
+    int stroke_points_count,
+    float x_range, 
+    float y_range, 
+    int width, 
+    int height,
+    int8_t* out_buffer);
+
---
+++ virtual_notepad_model_data.h
@@ -0,0 +1,3 @@
+extern const unsigned char g_virtual_notepad_model_data[];
+extern const int g_virtual_notepad_model_data_len;
+
